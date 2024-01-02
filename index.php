97ed925d95f44a53237c06acb01277f42d6caf89
Kseniia Oliveri <ksenia081096@gmail.com>
2024-01-02 13:49:02 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+
+use App\BuildReport;
+
+require __DIR__.'/vendor/autoload.php';
+
+$exp = new BuildReport;
+var_dump($exp->BuildReport());
---
+++ src/BuildReport.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App;
+
+use DateTimeImmutable;
+
+class BuildReport
+{
+public function BuildReport() 
+{   
+    $report = [];
+    foreach(file('./data/abbreviations.txt') as $lineNames)
+    {
+        $names = explode('_', $lineNames);
+
+        foreach(file('./data/end.log') as $lineEnd)
+        {
+            $driverEndTime = explode('_', $lineEnd);
+
+            foreach(file('./data/start.log') as $lineStart)
+            {
+                $driverStartTime = explode('_', $lineStart);
+                if($driverEndTime[0] == $driverStartTime[0])
+             {   
+               $startTimeMillisec = DateTimeImmutable::createFromFormat('H:i:s.u', $driverStartTime[1]);
+                $endTimeMillisec = DateTimeImmutable::createFromFormat('H:i:s.u', $driverEndTime[1]);
+                $timeDiff = $endTimeMillisec - $startTimeMillisec;
+            }
+        }
+
+    }
+
+        if($names[0] = $driverEndTime[0] )
+        {
+            $report[] = array(
+                'name' => $names[1],
+                'car' => $names[2],
+                'time' => date('i:s.v', $timeDiff));
+            
+        }
+    }
+    return $report;
+
+
+}
+   
+
+   
+}
