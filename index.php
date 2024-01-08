96cb58c1218fa7466d4dc5cfb3cb548b6eaffe25
Kseniia Oliveri <ksenia081096@gmail.com>
2024-01-08 19:04:58 +0100

    update

--- index.php
+++ index.php
@@ -1,34 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Report of Monaco 2018 Racing</title>
-    <style>
-table {
-  font-family: arial, sans-serif;
-  border-collapse: collapse;
-  width: 100%;
-}
+<?php
 
-td, th {
-  border: 1px solid #dddddd;
-  text-align: left;
-  padding: 8px;
-}
+use App\PrintReport;
+require __DIR__.'/vendor/autoload.php';
 
-tr:nth-child(even) {
-  background-color: #dddddd;
-}
-</style>
-</head>
-<body>
-    <h2>Report of Monaco 2018 Racing</h2>
 
-    <table>
-    
-        
-    </table>
-    
-</body>
-</html>
+$printReport = new PrintReport;
+$printReport->PrintReport();
--- src/BuildReport.php
+++ src/BuildReport.php
@@ -12,7 +12,6 @@ public function BuildReport()
     foreach(file('./data/abbreviations.txt') as $lineNames)
     {
         $names = explode('_', $lineNames);
-
         foreach(file('./data/end.log') as $lineEnd)
         {
             $driverEndTime = explode('_', $lineEnd);
@@ -23,8 +22,8 @@ public function BuildReport()
                 if($driverEndTime[0] == $driverStartTime[0])
              {   
                $startTimeMillisec = DateTimeImmutable::createFromFormat('H:i:s.u', $driverStartTime[1]);
-                $endTimeMillisec = DateTimeImmutable::createFromFormat('H:i:s.u', $driverEndTime[1]);
-                $timeDiff = $endTimeMillisec - $startTimeMillisec;
+               $endTimeMillisec = DateTimeImmutable::createFromFormat('H:i:s.u', $driverEndTime[1]);
+               $timeDiff = $endTimeMillisec - $startTimeMillisec;
              }
              }
 
@@ -32,16 +31,10 @@ public function BuildReport()
 
         if($names[0] = $driverEndTime[0] )
         {
-            $report[] = array(
-                'name' => $names[1],
-                'car' => $names[2],
-                'time' => date('i:s.v', $timeDiff));
-            
+            $report[] = array($names[1], trim($names[2]), date('i:s.v', $timeDiff));    
         }   
     }
     return $report;
 }
-   
-
-   
+     
 }
